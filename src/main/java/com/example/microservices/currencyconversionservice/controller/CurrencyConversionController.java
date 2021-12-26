c35117babcbc53e26a11ccccc2caff399c81a261
Kalidas R <laki3824@gmail.com>
2021-12-26 02:25:01 -0600

    Updated changes to use Feign/Eureka for naming server/loadbalancing

--- src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,16 +1,18 @@
 package com.example.microservices.currencyconversionservice.controller;
 
 import com.example.microservices.currencyconversionservice.entity.CurrencyConversion;
-import org.springframework.http.ResponseEntity;
+import com.example.microservices.currencyconversionservice.proxy.CurrentExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrentExchangeProxy currentExchangeProxy;
 
     @GetMapping(value = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -18,15 +20,7 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
 
-        HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
-                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                        CurrencyConversion.class, uriVariables);
-
-        CurrencyConversion currencyConversion = responseEntity.getBody();
+        CurrencyConversion currencyConversion = currentExchangeProxy.retrieveExchangeValue(from, to);
         currencyConversion.setQuantity(quantity);
         currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
         return currencyConversion;
